81a020de73784dbac4e7af87fe5b3a6d4bd5a3fe
Rutger Schoorstra <schoorstra_rutger@hotmail.com>
2013-03-04 14:41:48 +0100

    QuestionController Methodes Toegevoegd

--- MvcApplicationDatabase/Controllers/QuestionController.cs
+++ MvcApplicationDatabase/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcApplicationDatabase.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,29 @@ namespace MvcApplicationDatabase.Controllers
 {
     public class QuestionController : Controller
     {
-       // private StackOverflowDBContext db = new StackOverflowDBContext();
+        private StackOverflowDbContext db = new StackOverflowDbContext();
 
-        //
         // GET: /Question/
-
+        //
         public ActionResult Index()
         {
-            ViewBag.Test = "Test";
+            var Questions = db.Questions.OrderBy(q => q.DateCreated)
+                                        .Take(50); 
+
+            return View();
+        }
+
+        public ActionResult Edit(int Question_id)
+        {
+            var question = from q in db.Questions
+                           where q.Id == Question_id
+                           select q;
+
+            return View(question);
+        }
+
+        public ActionResult Ask()
+        {
 
             return View();
         }
