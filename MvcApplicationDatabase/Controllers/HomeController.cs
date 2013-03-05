867c926859a7b50ebf82279233eed3aaecf29c86
Rutger Schoorstra <Schoorstra_rutger@hotmail.com>
2013-03-05 16:06:55 +0100

    Summary field added to the Tags table

--- MvcApplicationDatabase/Controllers/HomeController.cs
+++ MvcApplicationDatabase/Controllers/HomeController.cs
@@ -15,7 +15,17 @@ namespace MvcApplicationDatabase.Controllers
         //
         public ActionResult Index()
         {
+            // Voorbeeld: Om alle tags op te vragen die bij een bepaalde question horen:
+            //
+            var tagList = db.Questions.ToList()[0].Tags;
+            // Werkt ook omgekeerd 
+            var randomTag = new Tag();
+            var QuestionList = randomTag.Questions.ToList();
+
+
+
             return View(db.Questions.ToList());
+            
         }
 
 
--- MvcApplicationDatabase/Models/Tag.cs
+++ MvcApplicationDatabase/Models/Tag.cs
@@ -21,6 +21,7 @@ namespace MvcApplicationDatabase.Models
     
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Summary { get; set; }
     
         public virtual ICollection<Question> Questions { get; set; }
     }
