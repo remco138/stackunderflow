6dd9acfb3a4c2d94e5dc2a9fa0a32bf531f3744a
Remco <remcodebruin@gmx.net>
2013-03-05 12:15:50 -0800

    Small stuff

--- MvcApplicationDatabase/Controllers/TagController.cs
+++ MvcApplicationDatabase/Controllers/TagController.cs
@@ -10,14 +10,7 @@ namespace MvcApplicationDatabase.Controllers
     public class TagController : Controller
     {
 
-            public struct Tag 
-            {
-                public string Name;
-                public string Summary;
-                public int Occurrences;
 
-                public Tag(string name, string summary, int occurrences) { Name = name; Summary = (summary == null) ? "I require a summary" : summary; Occurrences = occurrences; } 
-            }
 
 
         private StackOverflowDatabaseContext db = new StackOverflowDatabaseContext();
@@ -27,24 +20,10 @@ namespace MvcApplicationDatabase.Controllers
 
         public ActionResult Index()
         {
-            var dbTags = db.Tags.ToList();
-            List<Tag> tagList = new List<Tag>();
-
-            foreach (var t in dbTags)
-            {
-                tagList.Add(new Tag(t.Name, "I require an explanation", t.Questions.Count));
-            }
-
-            tagList.OrderByDescending(x => x.Occurrences);  //I've got not clue whether this works or not 
-
-            tagList.Add(new Tag("Lisp", "Pure elegancy", 9999));
-            tagList.Add(new Tag("C++", "A pretty cool language", 1337));
-            tagList.Add(new Tag("C#", "Microsoft(c)", 123));
-            tagList.Add(new Tag("VB", "Never heard of it. Disregard this very long text message supposedly explaining VB in detail .,.,gneweasdfghjkkl;asdfghjrwgfji4ker", -1));
-            tagList.Add(new Tag("C", "newline\ngoes\nhere", 12));
-
-
+            List<Tag> tagList = db.Tags.ToList();
+            tagList.OrderByDescending(x => x.Questions.Count);  //I've got not clue whether this works or not 
             ViewBag.tagList = tagList;
+ 
             return View();  //(tagNameList);
         }
 
