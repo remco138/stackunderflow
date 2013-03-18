36b05478923956e52b094c5274a505bf9d95bf85
minardus <minardus@gmail.com>
2013-03-18 15:15:25 +0100

    WhopWhop

--- MvcApplicationDatabase/Controllers/QuestionController.cs
+++ MvcApplicationDatabase/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
 ﻿using MvcApplicationDatabase.Models;
+using MvcApplicationDatabase.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -64,7 +65,7 @@ namespace MvcApplicationDatabase.Controllers
         }
 
         [HttpPost]
-        public ActionResult Ask(Question question, String tagnames)
+        public ActionResult Ask(Question question)
         {
             if (ModelState.IsValid)
             {
@@ -77,13 +78,27 @@ namespace MvcApplicationDatabase.Controllers
                 
                 // Was unable to add Tags, but fixed this by following the steps under 'Issues With Views': http://oyonti.wordpress.com/2011/05/26/unable-to-update-the-entityset-because-it-has-a-definingquery/
                 //
-                var tagNames = tagnames.Split(' ');
-                var tagList = db.Tags.Where(t => tagNames.Contains(t.Name)); // Find existing tags in database
+                string[] tagNames = Request.Form["Tag.Name"].Split(' ');
+                List<Tag> tagDb = db.Tags.Where(t => tagNames.Contains(t.Name)).ToList(); // Find existing tags in database
+                Tag[] tagList = new Tag[tagNames.Count()];
 
+                for (int i=0; i < tagNames.Count(); i++)
+                {
+                    tagList[i].Name = tagNames[i];
+                }
+
+                foreach (Tag tag in tagList)
+                {
+                    if(!tagDb.Contains(tag))
+                        question.Tags.Add(tag);
+                }
+
+                /*
                 foreach (Tag tag in tagList)
                 {
                     question.Tags.Add(tag);
                 }
+                 * */
 
                 db.Questions.Add(question);
                 db.SaveChanges();
--- MvcApplicationDatabase/Models/Question.cs
+++ MvcApplicationDatabase/Models/Question.cs
@@ -11,6 +11,7 @@ namespace MvcApplicationDatabase.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Question
     {
@@ -24,6 +25,7 @@ namespace MvcApplicationDatabase.Models
         public Nullable<int> OpeningPost_id { get; set; }
         public Nullable<int> BestAnswer_id { get; set; }
         public Nullable<System.DateTime> DateCreated { get; set; }
+        [Required]
         public string Title { get; set; }
         public Nullable<int> Views { get; set; }
     
