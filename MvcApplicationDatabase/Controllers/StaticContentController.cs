8549e879b8ea4c0986dc44ee03481d2372337665
minardus <minardus@gmail.com>
2013-03-21 14:38:01 +0100

    Added 404 error, when Page Is Not Found.
    Improved User Login Checks.
    
    YOU HAVE TO LOGIN WHEN ASKING QUESTION OR ANSWERING A QUESTION.
    
    Viva la capslock.

---
+++ MvcApplicationDatabase/Controllers/StaticContentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationDatabase.Controllers
+{
+    public class StaticContentController : Controller
+    {
+        //
+        // GET: /StaticContent/
+
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+    }
+}
