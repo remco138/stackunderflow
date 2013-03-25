2675d1336095ba8d412f48f09fcbad8c0140609e
minardus <minardus@gmail.com>
2013-03-25 15:20:09 +0100

    voting systeem test.
    werkt, alleen nog even afmaken.

--- MvcApplicationDatabase/Controllers/QuestionController.cs
+++ MvcApplicationDatabase/Controllers/QuestionController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using System.Data.Entity.Validation;
 using System.Diagnostics;
 using MarkdownSharp;
+using System.Web.Services;
 
 namespace MvcApplicationDatabase.Controllers
 {
@@ -164,6 +165,20 @@ namespace MvcApplicationDatabase.Controllers
         }
 
         
+        [WebMethod()]
+        //[ScriptMethod()]
+        public void Vote(int? id, string type = "up")
+        {
+            if (id != null)
+            {
+                var row = db.Posts.Where(p => p.Post_id == id).Single();
+                if (type == "up")
+                    row.Votes++;
+                else if(type == "down")
+                    row.Votes--;
+                db.SaveChanges();
+            }
+        }
 
     }
 }
