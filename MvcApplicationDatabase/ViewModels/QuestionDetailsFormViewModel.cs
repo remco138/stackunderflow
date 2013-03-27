620ae5edcaab2966e1c6c014a340f9f5995ace0b
minardus <minardus@gmail.com>
2013-03-27 10:34:11 +0100

    Database Update en wat cleanere code.

--- MvcApplicationDatabase/ViewModels/QuestionDetailsFormViewModel.cs
+++ MvcApplicationDatabase/ViewModels/QuestionDetailsFormViewModel.cs
@@ -13,13 +13,5 @@ namespace MvcApplicationDatabase.ViewModels
         public Post OpeningPost { get; set; }
         public Post BestAnswerPost { get; set; }
         public Post NewAnswer { get; set; }
-        public string[] GetUserDetails(int id)
-        {
-            string[] user = new string[2];
-            var record = db.Users.Where(u => u.User_id == id).Single();
-            user[0] = record.Username;
-            user[1] = record.Photo == null ? "photo.jpg" : record.Photo;
-            return user;
-        }
     }
 }
