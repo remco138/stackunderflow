4dc652769440ce28d222a5fd4d721937c10f04bd
Rutger Schoorstra <Schoorstra_rutger@hotmail.com>
2013-04-02 18:19:54 +0200

    Search Module Added

--- MvcApplicationDatabase/Controllers/SearchController.cs
+++ MvcApplicationDatabase/Controllers/SearchController.cs
@@ -1,21 +1,36 @@
-﻿using System;
+﻿using MvcApplicationDatabase.Models;
+using MvcApplicationDatabase.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using MarkdownSharp;
+using System.Web.Services;
 
 namespace MvcApplicationDatabase.Controllers
 {
     public class SearchController : Controller
     {
-        //
-        // GET: /Search/
+        private StackOverflowDatabaseContext db = new StackOverflowDatabaseContext();
 
-        public ActionResult Index()
+        // GET: /Search/
+        //
+        [HttpGet]
+        public ActionResult Index(String q)
         {
+            if (q == "") 
+                return View();
+
+            // Search questions where the title, question-content, or tag name contains the search string
+            //
+            ViewBag.QuestionList = db.Questions.Where(question => question.Title.Contains(q) ||
+                                                    question.Posts.FirstOrDefault().Content.Contains(q) ||
+                                                    question.Tags.Select(t => t.Name).Contains(q));
+
             return View();
         }
-
-
     }
 }
